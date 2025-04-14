a751fd417a6a5feba8f4d0026bfb6481c205784d
KauanLuna <kauan.luna@sptech.school>
2025-04-14 15:35:41 -0300

    corrigindo script e modelagem de banco de dados

--- Banco de Dados/Banco de dados AviTrackV3.sql
+++ Banco de Dados/Banco de dados AviTrackV3.sql
@@ -2,14 +2,13 @@ CREATE DATABASE avitrack;
 
 USE avitrack;
 
-CREATE TABLE usuario (
+CREATE TABLE pessoa_juridica (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45) NOT NULL,
+    razao_social VARCHAR(70) NOT NULL,
     email_comercial VARCHAR(60) NOT NULL,
     senha VARCHAR(30) NOT NULL,
     telefone_comercial CHAR(11) NOT NULL,
-    cpf CHAR(11) NOT NULL,
-    cnpj CHAR(14)
+    cnpj CHAR(14) NOT NULL
 );
 
 CREATE TABLE endereco (
@@ -20,10 +19,10 @@ CREATE TABLE endereco (
     cidade VARCHAR(45) NOT NULL,
     uf CHAR(2) NOT NULL,
     complemento VARCHAR(50),
-    fk_usuario INT,
-    CONSTRAINT fkUsuario FOREIGN KEY (fk_usuario) 
-		REFERENCES usuario(id),
-    CONSTRAINT pkCompostaEndereco PRIMARY KEY (id, fk_usuario)
+    fk_pj INT,
+    CONSTRAINT fkPessoaJuridica FOREIGN KEY (fk_pj) 
+		REFERENCES pessoa_juridica(id),
+    CONSTRAINT pkCompostaEndereco PRIMARY KEY (id, fk_pj)
 );
 
 CREATE TABLE aviario (
@@ -34,10 +33,10 @@ CREATE TABLE aviario (
     fk_endereco INT,
     CONSTRAINT fkEndereco FOREIGN KEY (fk_endereco) 
 		REFERENCES endereco(id),
-    fk_endereco_usuario INT,
-    CONSTRAINT fkEnderecoUsuario FOREIGN KEY (fk_endereco_usuario) 
-		REFERENCES endereco(fk_usuario),
-	CONSTRAINT pkCompostaAviario PRIMARY KEY (id, fk_endereco, fk_endereco_usuario)
+    fk_endereco_pj INT,
+    CONSTRAINT fkEnderecoPJ FOREIGN KEY (fk_endereco_pj) 
+		REFERENCES endereco(fk_pj),
+	CONSTRAINT pkCompostaAviario PRIMARY KEY (id, fk_endereco, fk_endereco_pj)
 );
 
 CREATE TABLE franganada (
@@ -56,13 +55,13 @@ CREATE TABLE setor(
 	fk_aviario_endereco INT,
     CONSTRAINT fkAviarioEndereco FOREIGN KEY (fk_aviario_endereco)
 		REFERENCES aviario(fk_endereco),
-    fk_aviario_endereco_usuario INT,
-    CONSTRAINT fkAviarioEnderecoUsuario FOREIGN KEY (fk_aviario_endereco_usuario)
-		REFERENCES aviario(fk_endereco_usuario),
+    fk_aviario_endereco_pj INT,
+    CONSTRAINT fkAviarioEnderecoPJ FOREIGN KEY (fk_aviario_endereco_pj)
+		REFERENCES aviario(fk_endereco_pj),
     fk_franganada INT,
     CONSTRAINT fkFranganada FOREIGN KEY (fk_franganada)
 		REFERENCES franganada(id),
-	CONSTRAINT pkCompostaSetor PRIMARY KEY (id, fk_aviario, fk_aviario_endereco, fk_aviario_endereco_usuario, fk_franganada)
+	CONSTRAINT pkCompostaSetor PRIMARY KEY (id, fk_aviario, fk_aviario_endereco, fk_aviario_endereco_pj, fk_franganada)
 );
 
 CREATE TABLE sensor (
@@ -79,9 +78,9 @@ CREATE TABLE sensor (
 	fk_setor_aviario_endereco INT,
     CONSTRAINT fkSetorAviarioEndereco FOREIGN KEY (fk_setor_aviario_endereco)
 		REFERENCES setor(fk_aviario_endereco),
-	fk_setor_aviario_endereco_usuario INT,
-    CONSTRAINT fkSetorAviarioEnderecoUsuario FOREIGN KEY (fk_setor_aviario_endereco_usuario)
-		REFERENCES setor(fk_aviario_endereco_usuario),
+	fk_setor_aviario_endereco_pj INT,
+    CONSTRAINT fkSetorAviarioEnderecoPJ FOREIGN KEY (fk_setor_aviario_endereco_pj)
+		REFERENCES setor(fk_aviario_endereco_pj),
 	fk_setor_franganada INT,
     CONSTRAINT fkSetorFranganada FOREIGN KEY (fk_setor_franganada)
 		REFERENCES setor(fk_franganada)
