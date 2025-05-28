b69108b4cce39f80052a3dee23d6552116792899
KauanLuna <kauan.luna@sptech.school>
2025-05-28 18:33:56 -0300

    Criando view no banco de dados, para selecionar dados dos aviários

--- Site Institucional/src/database/Banco de dados AviTrackV3.sql
+++ Site Institucional/src/database/Banco de dados AviTrackV3.sql
@@ -62,96 +62,138 @@ CREATE TABLE temperatura_celsius (
     CONSTRAINT pkCompostaDadoSensor PRIMARY KEY (id, fk_sensor)
 );
 
--- INSERT INTO pessoa_juridica (razao_social, email_comercial, senha, telefone_comercial, cnpj) VALUES 
--- ('Granja Bom Frango', 'contato@bomfrango.comt.br', 'senha123', '11987654321', '12345678000199'),
--- ('Aviários Vale Verde', 'suporte@valeverde.comt.br', 'vale2024', '1133445566', '98765432000188'),
--- ('Frango Feliz LTDA', 'comercial@frangofeliz.com', 'feliz2025', '1199887766', '45612378000166');
+INSERT INTO pessoa_juridica (razao_social, email_comercial, senha, telefone_comercial, cnpj) VALUES 
+('Granja Bom Frango', 'contato@bomfrango.comt.br', 'senha123', '11987654321', '12345678000199'),
+('Aviários Vale Verde', 'suporte@valeverde.comt.br', 'vale2024', '1133445566', '98765432000188'),
+('Frango Feliz LTDA', 'comercial@frangofeliz.com', 'feliz2025', '1199887766', '45612378000166');
 
--- INSERT INTO aviario (id, nome, qtd_frangos, qtd_frangos_mortos, cep, logradouro, numero, cidade, uf, complemento, fk_pessoa_juridica) VALUES 
--- (1, 'Aviário Central', 1000, 12, '12345678', 'Estrada da Granja', '150', 'São Paulo', 'SP', 'Próximo ao silo', 1),
--- (2, 'Unidade Vale Norte', 800, 5, '87654321', 'Rodovia Verde', '300', 'Campinas', 'SP', 'Km 25', 2),
--- (3, 'Polo Feliz Oeste', 1200, 20, '11223344', 'Av. das Aves', '789', 'Ribeirão Preto', 'SP', NULL, 3);
+INSERT INTO aviario (id, nome, qtd_frangos, qtd_frangos_mortos, cep, logradouro, numero, cidade, uf, complemento, fk_pessoa_juridica) VALUES 
+(1, 'Aviário Central', 1000, 12, '12345678', 'Estrada da Granja', '150', 'São Paulo', 'SP', 'Próximo ao silo', 1),
+(2, 'Unidade Vale Norte', 800, 5, '87654321', 'Rodovia Verde', '300', 'Campinas', 'SP', 'Km 25', 2),
+(3, 'Polo Feliz Oeste', 1200, 20, '11223344', 'Av. das Aves', '789', 'Ribeirão Preto', 'SP', NULL, 3);
 
--- INSERT INTO setor (id, nome, fk_aviario) VALUES 
--- (1, 'Setor A1', 1),
--- (2, 'Setor A2', 1),
--- (3, 'Setor A3', 1),
+INSERT INTO setor (id, nome, fk_aviario) VALUES 
+(1, 'Setor A1', 1),
+(2, 'Setor A2', 1),
+(3, 'Setor A3', 1),
 
--- (1, 'Setor B1', 2),
--- (2, 'Setor B2', 2),
--- (3, 'Setor B3', 2),
+(1, 'Setor B1', 2),
+(2, 'Setor B2', 2),
+(3, 'Setor B3', 2),
 
--- (1, 'Setor C1', 3),
--- (2, 'Setor C2', 3),
--- (3, 'Setor C3', 3);
+(1, 'Setor C1', 3),
+(2, 'Setor C2', 3),
+(3, 'Setor C3', 3);
 
--- INSERT INTO sensor (modelo, tipo_leitura, num_serie, fk_setor, fk_setor_aviario) VALUES 
--- ('LM35', 'Temperatura', 'LM35-A1-001', 1, 1),
--- ('LM35', 'Temperatura', 'LM35-A2-002', 2, 1),
--- ('LM35', 'Temperatura', 'LM35-A3-003', 3, 1),
+INSERT INTO sensor (modelo, tipo_leitura, num_serie, fk_setor, fk_setor_aviario) VALUES 
+('LM35', 'Temperatura', 'LM35-A1-001', 1, 1),
+('LM35', 'Temperatura', 'LM35-A2-002', 2, 1),
+('LM35', 'Temperatura', 'LM35-A3-003', 3, 1),
 
--- ('LM35', 'Temperatura', 'LM35-B1-004', 1, 2),
--- ('LM35', 'Temperatura', 'LM35-B2-005', 2, 2),
--- ('LM35', 'Temperatura', 'LM35-B3-006', 3, 2),
+('LM35', 'Temperatura', 'LM35-B1-004', 1, 2),
+('LM35', 'Temperatura', 'LM35-B2-005', 2, 2),
+('LM35', 'Temperatura', 'LM35-B3-006', 3, 2),
 
--- ('LM35', 'Temperatura', 'LM35-C1-007', 1, 3),
--- ('LM35', 'Temperatura', 'LM35-C2-008', 2, 3),
--- ('LM35', 'Temperatura', 'LM35-C3-009', 3, 3);
+('LM35', 'Temperatura', 'LM35-C1-007', 1, 3),
+('LM35', 'Temperatura', 'LM35-C2-008', 2, 3),
+('LM35', 'Temperatura', 'LM35-C3-009', 3, 3);
 
--- INSERT INTO temperatura_celsius (medicao, dt_hora, fk_sensor) VALUES
--- (28.5, '2025-04-22 08:00:00', 1),
--- (29.1, '2025-04-22 08:05:00', 2),
--- (27.8, '2025-04-22 08:10:00', 3);
+INSERT INTO temperatura_celsius (medicao, dt_hora, fk_sensor) VALUES
+(28.5, '2025-04-22 08:00:00', 1),
+(29.1, '2025-04-22 08:05:00', 2),
+(27.8, '2025-04-22 08:10:00', 3);
 
--- SELECT * FROM pessoa_juridica;
+SELECT * FROM pessoa_juridica;
 
--- SELECT * FROM aviario;
+SELECT * FROM aviario;
 
--- SELECT * FROM setor;
+SELECT * FROM setor;
 
--- SELECT * FROM sensor;
+SELECT * FROM sensor;
 
--- SELECT * FROM temperatura_celsius;
+SELECT * FROM temperatura_celsius;
 
--- SELECT a.nome AS Aviário,
--- 	a.qtd_frangos AS 'Quantidade de Frangos',
---     a.cep AS CEP,
---     CONCAT(a.logradouro, ' ', a.numero, ' ', a.cidade, ' ', a.uf) AS Endereço,
---     IFNULL(a.complemento, 'Sem complemento') as Complemento,
---     pj.razao_social AS 'Usuário dono'
---     FROM aviario AS a 
--- 	JOIN pessoa_juridica AS pj 
--- 		ON a.fk_pessoa_juridica = pj.id;
+SELECT a.nome AS Aviário,
+	a.qtd_frangos AS 'Quantidade de Frangos',
+    a.cep AS CEP,
+    CONCAT(a.logradouro, ' ', a.numero, ' ', a.cidade, ' ', a.uf) AS Endereço,
+    IFNULL(a.complemento, 'Sem complemento') as Complemento,
+    pj.razao_social AS 'Usuário dono'
+    FROM aviario AS a 
+	JOIN pessoa_juridica AS pj 
+		ON a.fk_pessoa_juridica = pj.id;
 
--- SELECT pj.razao_social as 'Razão Social',
--- 	CONCAT(pj.email_comercial, ' - ', pj.telefone_comercial) as Contatos,
---     pj.cnpj as CNPJ
---     FROM pessoa_juridica as pj;
+SELECT pj.razao_social as 'Razão Social',
+	CONCAT(pj.email_comercial, ' - ', pj.telefone_comercial) as Contatos,
+    pj.cnpj as CNPJ
+    FROM pessoa_juridica as pj;
 
--- SELECT sensor.modelo as Modelo,
--- 	sensor.tipo_leitura as 'Tipo de leitura',
---     sensor.num_serie 'Número de série',
---     CONCAT(aviario.nome, ' - ', setor.nome) as 'Aviário & Setor'
---     FROM sensor
---     JOIN setor
--- 		ON sensor.fk_setor = setor.id
--- 	JOIN aviario
--- 		ON sensor.fk_setor_aviario = aviario.id;
---         
--- SELECT mt.temperatura as Temperatura,
--- 	date_format(mt.dt_hora, '%d/%m/%Y - %H:%i') as 'Data & Hora',
---     sensor.modelo as 'Modelo do sensor',
---     sensor.num_serie as 'Número de série',
---     CONCAT(aviario.nome, ' - ', setor.nome) as 'Aviário & Setor',
---     CASE
--- 		WHEN mt.temperatura < 19 THEN 'Abaixo do ideal'
---         WHEN mt.temperatura > 28 THEN 'Acima do ideal'
---         ELSE 'Temperatura ideal'
--- 	END as Situação
---     FROM medicao_temperatura as mt
---     JOIN sensor
--- 		ON mt.fk_sensor = sensor.id
--- 	JOIN setor
--- 		ON sensor.fk_setor = setor.id
--- 	JOIN aviario
--- 		ON setor.fk_aviario = aviario.id order by mt.dt_hora asc;
+SELECT sensor.modelo as Modelo,
+	sensor.tipo_leitura as 'Tipo de leitura',
+    sensor.num_serie 'Número de série',
+    CONCAT(aviario.nome, ' - ', setor.nome) as 'Aviário & Setor'
+    FROM sensor
+    JOIN setor
+		ON sensor.fk_setor = setor.id
+	JOIN aviario
+		ON sensor.fk_setor_aviario = aviario.id;
+        
+SELECT tc.medicao as Temperatura,
+	date_format(tc.dt_hora, '%d/%m/%Y - %H:%i') as 'Data & Hora',
+    sensor.modelo as 'Modelo do sensor',
+    sensor.num_serie as 'Número de série',
+    CONCAT(aviario.nome, ' - ', setor.nome) as 'Aviário & Setor',
+    CASE
+		WHEN tc.medicao < 19 THEN 'Abaixo do ideal'
+        WHEN tc.medicao > 28 THEN 'Acima do ideal'
+        ELSE 'Temperatura ideal'
+	END as Situação
+    FROM temperatura_celsius tc
+    JOIN sensor
+		ON tc.fk_sensor = sensor.id
+	JOIN setor
+		ON sensor.fk_setor = setor.id
+	JOIN aviario
+		ON setor.fk_aviario = aviario.id order by tc.dt_hora asc;
+        
+        -- Temperatura agrupada por cada aviário
+CREATE VIEW dadosAviario 
+AS
+SELECT 
+	a.nome AviarioNome,
+    s.nome SetorNome,
+    sen.modelo ModeloSensor,
+    tc.medicao Temperatura
+FROM 
+	aviario a
+JOIN 
+	setor s
+ON 
+	s.fk_aviario = a.id
+JOIN
+	sensor sen
+ON 
+	sen.fk_setor = s.id
+JOIN 
+	temperatura_celsius tc
+ON
+	sen.id = tc.fk_sensor
+JOIN 
+	pessoa_juridica  pj
+ON
+	pj.id = a.fk_pessoa_juridica
+GROUP BY
+	a.nome,
+    s.nome,
+    sen.modelo,
+    tc.medicao;
+    
+SELECT * FROM dadosAviario;
+
+
+
+
+
+
+
+
